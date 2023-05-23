a98842a99fb1b69c97b4447de7d548cd6e0adcad
APPactivities <drakopoulouevangelia@gmail.com>
2023-05-23 16:44:19 +0300

    checking gridpoint

--- check_gridPoint/LocatePredictedVertex.py
+++ check_gridPoint/LocatePredictedVertex.py
@@ -35,20 +35,28 @@ def find_tank(truevtxx_tank, truevtxy_tank, truevtxz_tank, step, Gridpoint):
                 i += 1
 
     # find vtx coordinates for given gridpoint
-    z, r, c = gridpoint[int(Gridpoint)]
-    vtx_z = -140 + z * step
-    vtx_y = -190 + r * step
-    vtx_x = -140 + c * step
+    if Gridpoint<=36000:
+       z, r, c = gridpoint[int(Gridpoint)]
+       vtx_z = -140 + z * step
+       vtx_y = -190 + r * step
+       vtx_x = -140 + c * step
+    else: 
+       vtx_z = -999
+       vtx_y = -999
+       vtx_x = -999
 
     return vtx_x, vtx_y, vtx_z
 
 
 # check the length of the DataFrame
 print(df.shape)
+#print(df.describe())
 
 # apply find_tank function to each row
-new_columns = df['Gridpoint'].apply(lambda gp: find_tank(0, 0, 0, 10, gp))
-print("checking vertex for gridpoint 15044 : ", find_tank(0, 0, 0, 10, 15044))
+#print("Max gridpoint:",df['Gridpoint'].max())
+new_columns = df['Predicted_Gridpoint'].apply(lambda gp: find_tank(0, 0, 0, 10, gp))
+
+print("checking vertex for gridpoint 15044 : ", find_tank(0, 0, 0, 10, 15044.44))
 print("checking vertex for gridpoint 15078 : ", find_tank(0, 0, 0, 10, 15078))
 print("checking vertex for gridpoint 15079 : ", find_tank(0, 0, 0, 10, 15079))
 
@@ -96,9 +104,9 @@ pylab.rcParams.update(params)
 
 data = df_final['DR_DNN']
 dataprev = df_final['DR_reco']
-print("data ",data)
-print("dataprev ",dataprev)
-nbins=np.arange(0,100,1)
+#print("data ",data)
+#print("dataprev ",dataprev)
+nbins=np.arange(0,400,4)
 fig,ax=plt.subplots(ncols=1, sharey=False)#, figsize=(8, 6))
 f0=ax.hist(data, nbins, histtype='step', fill=False, color='blue',alpha=0.75)
 f1=ax.hist(dataprev, nbins, histtype='step', fill=False, color='red',alpha=0.75)
@@ -113,3 +121,53 @@ plt.show()
 fig.savefig('GridPoint_resol_DR.png')
 plt.close(fig)
 
+#plot Gridpoint difference histos:
+data0 = df_final['Gridpoint']-df['Predicted_Gridpoint']
+dataprev0 = df_final['Gridpoint']-df['Reco_Gridpoint']
+print("data0 ",data0)
+print("dataprev0 ",dataprev0)
+nbins=np.arange(0,1000,100)
+fig,ax=plt.subplots(ncols=1, sharey=False)#, figsize=(8, 6))
+f0=ax.hist(data0, nbins, histtype='step', fill=False, color='blue',alpha=0.75)
+f1=ax.hist(dataprev0, nbins, histtype='step', fill=False, color='red',alpha=0.75)
+#ax.set_xlim(0.,200.)
+ax.set_xlabel('grispoint Difference')
+ax.legend(('DNN-True gridpoint','Reco-True gridpoint'))
+#ax.xaxis.set_ticks(np.arange(0., 500., 50))
+ax.tick_params(axis='x', which='minor', bottom=False)
+title = "mean = %.2f, std = %.2f, Prev: mean = %.2f, std = %.2f " % (data0.mean(), data0.std(),dataprev0.mean(), dataprev0.std())
+plt.title(title)
+plt.show()
+fig.savefig('GridPoint_resol.png')
+plt.close(fig)
+
+#plot true vs DNN predicted gridpoint:
+import ROOT
+canvas = ROOT.TCanvas()
+canvas.cd(1)
+th2f = ROOT.TH2F("DNN_GridPoint", "; GridPoint; DNN GridPoint prediction", 150, 5000, 20000., 150, 5000., 20000.)
+for i in range(len(df['Gridpoint'])):
+    th2f.Fill(df['Gridpoint'][i], df['Predicted_Gridpoint'][i])
+#line = ROOT.TLine(20000.,20000.,20000.,20000.)
+th2f.SetStats(0)
+th2f.Draw("ColZ")
+#line.SetLineColor(2)
+canvas.Draw()
+#line.Draw("same")
+canvas.SaveAs("DNN_true_Gridpoint.png")
+
+
+#plot true vs reco gridpoint:
+import ROOT
+canvas = ROOT.TCanvas()
+canvas.cd(1)
+th2f = ROOT.TH2F("Reco_GridPoint", "; GridPoint; GridPoint from Reco.vertex", 150, 5000, 20000., 150, 5000., 20000.)
+for i in range(len(df['Gridpoint'])):
+    th2f.Fill(df['Gridpoint'][i], df['Reco_Gridpoint'][i])
+#line = ROOT.TLine(20000.,20000.,20000.,20000.)
+th2f.SetStats(0)
+th2f.Draw("ColZ")
+#line.SetLineColor(2)
+canvas.Draw()
+#line.Draw("same")
+canvas.SaveAs("reco_true_Gridpoint.png")
